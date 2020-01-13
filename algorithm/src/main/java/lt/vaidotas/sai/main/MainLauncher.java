18f3676d6e11d9b084c643fd056cb48937a80b08
vaidotas <vaidotas@smthsmth.com>
2020-01-13 10:55:01 +0200

    First commit

---
+++ algorithm/src/main/java/lt/vaidotas/sai/main/MainLauncher.java
@@ -0,0 +1,11 @@
+package lt.vaidotas.sai.main;
+
+public class MainLauncher {
+
+    /**
+     * main method, which handles user interface
+     * @param args
+     */
+    public static void main(String[] args) {
+    }
+}
